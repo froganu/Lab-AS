96feca7d2a9a9bc007ea569588eb2e6f301519fc
jsolegar <jordi.20017@gmail.com>
2025-11-18 18:32:37 +0100

    Backend access removed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
-import routes from "./routes/index.js"; //route general
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename); //estas dos lineas se eliminaran cuando se quite el html
+import routes from "./routes/index.js"; // Solo API
 
 const app = express();
 
@@ -14,14 +9,11 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-// servir archivos estáticos (HTML, CSS, JS)
-app.use(express.static(path.join(__dirname, "public"))); // se elimina cuando se quite el html
-
 app.use("/api", routes);
 
-// Redirección inicial dependiendo de si hay sesión
+// Si quieres una redirección automática puedes hacer:
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "redirect.html")); //cuando se quite el html, pues tambien se quitara esto pero la logica deberia ser la misma
+  res.json({ message: "API running" });
 });
 
 export default app;
